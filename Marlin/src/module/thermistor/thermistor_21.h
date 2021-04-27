41ba231507c7638341a7ad3d0c910444a822166c
Bryan <gruvin@gmail.com>
2021-04-28 11:40:11 +1200

    Merge https://github.com/makerbase-mks/Marlin-V2.X-MKS-H43 repo into gruvin/Marlin Tested working with Ender 3 V2 DWIN display.

--- Marlin/src/module/thermistor/thermistor_21.h
+++ Marlin/src/module/thermistor/thermistor_21.h
@@ -1,6 +1,6 @@
-/*
+/**
  * Marlin 3D Printer Firmware
- * Copyright (c) 2019 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ * Copyright (c) 2020 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
  *
  * Based on Sprinter and grbl.
  * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
@@ -16,100 +16,63 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
  */
 #pragma once
 
-// PT100 with E3D INA826 amp on 3.3V SKR 1.3 (Smoothieboard)
-/// *** SOME TEMPS EXTRAPOLATED *** See table below.
-// The PT100 in the Ultimaker v2.0 electronics has a high sample value for a high temperature.
-// This does not match the normal thermistor behavior so we need to set the following defines
-#if THERMISTOR_HEATER_0 == 21
-  #define HEATER_0_RAW_HI_TEMP 16383
-  #define HEATER_0_RAW_LO_TEMP 0
-#endif
-#if THERMISTOR_HEATER_1 == 21
-  #define HEATER_1_RAW_HI_TEMP 16383
-  #define HEATER_1_RAW_LO_TEMP 0
-#endif
-#if THERMISTOR_HEATER_2 == 21
-  #define HEATER_2_RAW_HI_TEMP 16383
-  #define HEATER_2_RAW_LO_TEMP 0
-#endif
-#if THERMISTOR_HEATER_3 == 21
-  #define HEATER_3_RAW_HI_TEMP 16383
-  #define HEATER_3_RAW_LO_TEMP 0
-#endif
-#if THERMISTOR_HEATER_4 == 21
-  #define HEATER_4_RAW_HI_TEMP 16383
-  #define HEATER_4_RAW_LO_TEMP 0
-#endif
-#if THERMISTOR_HEATER_5 == 21
-  #define HEATER_5_RAW_HI_TEMP 16383
-  #define HEATER_5_RAW_LO_TEMP 0
-#endif
-#if THERMISTORBED == 21
-  #define HEATER_BED_RAW_HI_TEMP 16383
-  #define HEATER_BED_RAW_LO_TEMP 0
-#endif
-#if THERMISTORCHAMBER == 21
-  #define HEATER_CHAMBER_RAW_HI_TEMP 16383
-  #define HEATER_CHAMBER_RAW_LO_TEMP 0
-#endif
-const short temptable_21[][2] PROGMEM = {
-  { OV(  0),    0 },  // NA
-  { OV(227),    1 },  // EXTRAPOLATED
-  { OV(236),   10 },  // EXTRAPOLATED
-  { OV(245),   20 },  // EXTRAPOLATED
-  { OV(253),   30 },  // EXTRAPOLATED
-  { OV(262),   40 },  // MEASURED
-  { OV(270),   50 },  // MEASURED
-  { OV(275),   60 },  // MEASURED
-  { OV(285),   70 },  // MEASURED
-  { OV(294),   80 },  // MEASURED
-  { OV(302),   90 },  // MEASURED
-  { OV(310),  100 },  // MEASURED
-  { OV(318),  110 },  // MEASURED
-  { OV(326),  120 },  // MEASURED
-  { OV(335),  130 },  // MEASURED
-  { OV(342),  140 },  // MEASURED
-  { OV(351),  150 },  // MEASURED
-  { OV(359),  160 },  // MEASURED
-  { OV(367),  170 },  // MEASURED
-  { OV(375),  180 },  // MEASURED
-  { OV(383),  190 },  // MEASURED
-  { OV(391),  200 },  // MEASURED
-  { OV(399),  210 },  // MEASURED
-  { OV(407),  220 },  // MEASURED
-  { OV(415),  230 },  // MEASURED
-  { OV(423),  240 },  // MEASURED
-  { OV(432),  250 },  // MEASURED
-  { OV(440),  260 },  // MEASURED
-  { OV(447),  270 },  // MEASURED
-  { OV(454),  280 },  // MEASURED
-  { OV(462),  290 },  // MEASURED
-  { OV(470),  300 },  // MEASURED
-  { OV(478),  310 },  // MEASURED
-  { OV(485),  320 },  // MEASURED
-  { OV(492),  330 },  // MEASURED
-  { OV(499),  340 },  // MEASURED
-  { OV(507),  350 },  // MEASURED
-  { OV(514),  360 },  // MEASURED
-  { OV(520),  370 },  // MEASURED
-  { OV(522),  380 },  // MEASURED
-  { OV(524),  390 },  // MEASURED
-  { OV(526),  400 },  // MEASURED
-  { OV(528),  410 },  // MEASURED
-  { OV(531),  420 },  // MEASURED
-  { OV(533),  430 },  // MEASURED
-  { OV(535),  440 },  // MEASURED
-  { OV(537),  450 },  // MEASURED
-  { OV(614),  500 },  // ??
-  { OV(681),  600 },  // ??
-  { OV(744),  700 },  // ??
-  { OV(805),  800 },  // ??
-  { OV(862),  900 },  // ??
-  { OV(917), 1000 },  // ??
-  { OV(968), 1100 }   // ??
+#define REVERSE_TEMP_SENSOR_RANGE_21 1
+
+#undef OV_SCALE
+#define OV_SCALE(N) (float((N) * 5) / 3.3f)
+
+// Pt100 with INA826 amplifier board with 5v supply based on Thermistor 20, with 3v3 ADC reference on the mainboard.
+// If the ADC reference and INA826 board supply voltage are identical, Thermistor 20 instead.
+const temp_entry_t temptable_21[] PROGMEM = {
+  { OV(  0),    0 },
+  { OV(227),    1 },
+  { OV(236),   10 },
+  { OV(245),   20 },
+  { OV(253),   30 },
+  { OV(262),   40 },
+  { OV(270),   50 },
+  { OV(279),   60 },
+  { OV(287),   70 },
+  { OV(295),   80 },
+  { OV(304),   90 },
+  { OV(312),  100 },
+  { OV(320),  110 },
+  { OV(329),  120 },
+  { OV(337),  130 },
+  { OV(345),  140 },
+  { OV(353),  150 },
+  { OV(361),  160 },
+  { OV(369),  170 },
+  { OV(377),  180 },
+  { OV(385),  190 },
+  { OV(393),  200 },
+  { OV(401),  210 },
+  { OV(409),  220 },
+  { OV(417),  230 },
+  { OV(424),  240 },
+  { OV(432),  250 },
+  { OV(440),  260 },
+  { OV(447),  270 },
+  { OV(455),  280 },
+  { OV(463),  290 },
+  { OV(470),  300 },
+  { OV(478),  310 },
+  { OV(485),  320 },
+  { OV(493),  330 },
+  { OV(500),  340 },
+  { OV(507),  350 },
+  { OV(515),  360 },
+  { OV(522),  370 },
+  { OV(529),  380 },
+  { OV(537),  390 },
+  { OV(544),  400 },
+  { OV(614),  500 }
 };
+
+#undef OV_SCALE
+#define OV_SCALE(N) (N)
--- Marlin/src/pins/ramps/pins_MKS_GEN_L.h
+++ Marlin/src/pins/ramps/pins_MKS_GEN_L.h
@@ -1,6 +1,6 @@
 /**
  * Marlin 3D Printer Firmware
- * Copyright (c) 2019 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ * Copyright (c) 2020 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
  *
  * Based on Sprinter and grbl.
  * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
@@ -16,7 +16,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
  */
 #pragma once
@@ -51,3 +51,9 @@
 #endif
 
 #include "pins_RAMPS.h"
+
+#if ENABLED(DGUS_LCD_UI_MKS)
+  #ifndef SD_DETECT_PIN
+   #define SD_DETECT_PIN 49
+ #endif
+#endif
