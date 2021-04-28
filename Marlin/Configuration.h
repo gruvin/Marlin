3e75b764fb77f938a388636b17bb89211b5c1cac
Bryan <gruvin@gmail.com>
2021-04-28 12:42:21 +1200

    Correct Z homing slow speed.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -995,10 +995,10 @@
 #define XY_PROBE_SPEED 6000
 
 // Feedrate (mm/min) for the first approach when double-probing (MULTIPLE_PROBING == 2)
-#define Z_PROBE_SPEED_FAST (600)
+#define Z_PROBE_SPEED_FAST 600
 
 // Feedrate (mm/min) for the "accurate" probe of each point
-#define Z_PROBE_SPEED_SLOW (Z_PROBE_SPEED_FAST / 2)
+#define Z_PROBE_SPEED_SLOW (Z_PROBE_SPEED_FAST / 4)
 
 /**
  * Probe Activation Switch
